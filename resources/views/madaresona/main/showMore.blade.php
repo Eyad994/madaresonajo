864d5d44b5ba2ab33ea0015b4b5851bb9a91b1e4
OmarMarie <omarmaree97@gmail.com>
2020-07-31 09:39:27 +0300

    add

--- resources/views/madaresona/main/showMore.blade.php
+++ resources/views/madaresona/main/showMore.blade.php
@@ -7,7 +7,6 @@
             style="direction:ltr; text-align:left;">
         @endif
         <div class="container">
-            <div class="row">
             <div class="title_news rounded" style="margin-top: 25px; background-color: #fff;  padding: 15px 50px; box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.12), 0 1px 5px 0 rgba(0, 0, 0, 0.2);">
                 <h2 style="font-weight: bold">{{(app()->getLocale() == 'en') ? $news->title_en :$news->title_ar}}</h2>
                 <p style="border: 2px solid #eaeaea;"></p>
@@ -17,7 +16,6 @@
                     <i class="fad fa-clock"></i>
                         {{ $news->created_at->format('d M Y - g:i:s A')}}
                 </time>
-            </div>
             </div>
             <div class="row">
                 <div class="col-md-12 " style="padding: 15px; text-align: center; ">
@@ -36,7 +34,7 @@
 
             @if(isset($news->youtube))
                 <div class="rounded" style="margin-top: 15px; text-align: center; ">
-                    <iframe class="rounded" width="75%" height="400px" src="{{$news->youtube}}"
+                    <iframe class="rounded" width="75%" height="400px" src="https://www.youtube.com/embed/{{$news->youtube}}"
                             frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope;
                              picture-in-picture" allowfullscreen></iframe>
 
