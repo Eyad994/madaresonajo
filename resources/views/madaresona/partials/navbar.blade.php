fafd855d57723daee035e82a5d4dbf4aea8e8e4e
OmarMarie <omarmaree97@gmail.com>
2020-07-31 22:24:21 +0300

    add

--- resources/views/madaresona/partials/navbar.blade.php
+++ resources/views/madaresona/partials/navbar.blade.php
@@ -32,7 +32,7 @@
                     </li>
                     <li class="item-101 selected "><a class=" js-activated bt-white font-weight-bold" href="http://www.josmsservice.com/madaresona/" target="_blank">{{ __('index.sms_platform') }}</a></li>
                     <li class="item-101 selected "><a href="{{ request()->segment(2) == null ? '#services' : route('home').'#services' }}" class=" js-activated bt-white font-weight-bold">{{ __('index.services') }}</a></li>
-                    <li class="item-101 selected "><a class=" js-activated bt-white font-weight-bold">{{ __('index.madaresona_jobs') }}</a></li>
+                    <li class="item-101 selected "><a class=" js-activated bt-white font-weight-bold" href="http://job.madaresonajo.com">{{ __('index.madaresona_jobs') }}</a></li>
 
                     <li class="item-101 selected "><a class=" js-activated bt-white font-weight-bold"  href="{{ route('discounts', app()->getLocale()) }}"> {{ __('index.discounts') }}</a></li>
                     <li class="item-101 selected "><a class=" js-activated bt-white font-weight-bold"  href="{{ route('home', app()->getLocale()) }}"> {{ __('index.suppliers') }}</a></li>
