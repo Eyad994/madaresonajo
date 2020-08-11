5b16791301dfdd62b31b87fd630cd128b21db8e6
OmarMarie <omarmaree97@gmail.com>
2020-08-11 17:33:53 +0300

    add

--- resources/views/ads.blade.php
+++ resources/views/ads.blade.php
@@ -1,7 +1,7 @@
 @if(count($ads) > 0)
     @foreach($ads as $ad)
-        <a href="{{ $ad->link }}" style="box-shadow: 0 2px 4px 0 rgba(0,0,0,.16),0 2px 10px 0 rgba(0,0,0,.12)!important;margin-top: 50px; !important;border-radius: 10px;">
+        <div href="{{ $ad->link }}" style="box-shadow: 0 2px 4px 0 rgba(0,0,0,.16),0 2px 10px 0 rgba(0,0,0,.12)!important; !important;border-radius: 10px; margin-top: 50px;">
              <img src="{{ env('IMAGE_URL') }}/images/Advertisement/{{ $ad->img }}" style="width: 100%;border-radius: 10px;">
-        </a>
+        </div>
     @endforeach
 @endif
