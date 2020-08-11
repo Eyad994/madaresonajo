5babbb8faff03691a3dbf0d18aff5909fd4f6654
OmarMarie <omarmaree97@gmail.com>
2020-08-11 17:37:42 +0300

    add

--- resources/views/ads.blade.php
+++ resources/views/ads.blade.php
@@ -2,7 +2,7 @@
     @foreach($ads as $ad)
 
         <div style="box-shadow: 0 2px 4px 0 rgba(0,0,0,.16),0 2px 10px 0 rgba(0,0,0,.12)!important; !important;border-radius: 10px; margin-top: 50px;">
-            <a href="{{ $ad->link }}">
+            <a href="{{ $ad->url }}">
                 <img src="{{ env('IMAGE_URL') }}/images/Advertisement/{{ $ad->img }}"
                      style="width: 100%;border-radius: 10px;">
             </a>
