49ed6d7fd3bc2d280b864f1ee39b4b4a1926d1b3
OmarMarie <omarmaree97@gmail.com>
2020-08-18 01:12:12 +0300

    add

--- resources/views/madaresona/main/showWithoutModal.blade.php
+++ resources/views/madaresona/main/showWithoutModal.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.main')
 <meta property="og:url"           content="{{ env('MADARESONA_URL') }}/{{ app()->getLocale() }}/school-data/{{$school->id}}/{{ preg_replace('/[ ]+/', '-', app()->getLocale() == 'en' ? trim($school->name_en) : trim($school->name_ar)) }}" />
 <meta property="og:title"         content="{{(app()->getLocale() == 'ar') ?$school->name_ar :$school->name_en }}" />
+<meta property="og:description"   content="{{app()->getLocale() == 'en' ? $school->name_en : $school->name_ar}}">
 <meta property="og:image"         content="{{ env('IMAGE_URL') }}/images/{{ $school->name_en }}/{{ $school->school_logo }}" />
+<meta property="og:type" content="article" />
 <meta name="description" content="{{app()->getLocale() == 'en' ? $school->name_en : $school->name_ar}}">
 <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}"/>
 <style>
