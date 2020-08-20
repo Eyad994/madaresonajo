5820a3664dc12bdb74d504d28bfc5daf19439cd5
OmarMarie <omarmaree97@gmail.com>
2020-08-20 15:02:50 +0300

    add

--- app/Http/Controllers/SchoolController.php
+++ app/Http/Controllers/SchoolController.php
@@ -109,8 +109,6 @@ class SchoolController extends Controller
         App::setLocale($locale);
         $cleanSlug = str_replace('-', ' ', $slug);
         $cleanSlug = rtrim($cleanSlug);
-        // preg_replace('/[-]+/', ' ', trim($cleanSlug))
-
         $school = School::where('name_ar', 'like', "%$cleanSlug%")->orWhere('name_en', 'like', "%$cleanSlug%")->first();
         $genderSchool = explode(',', $school->gender);
         $gallery = GallarySchool::where('school_id', $school->id)->get();
@@ -127,10 +125,8 @@ class SchoolController extends Controller
         App::setLocale($locale);
         $cleanSlug = str_replace('-', ' ', $slug);
         $cleanSlug = rtrim($cleanSlug);
-        // preg_replace('/[-]+/', ' ', trim($cleanSlug))
-
         $school = School::where('name_ar', 'like', "%$cleanSlug%")->orWhere('name_en', 'like', "%$cleanSlug%")->first();
-        $genderSchool = explode(',', $school->gender);
+       /* $genderSchool = explode(',', $school->gender);*/
         $gallery = GallarySchool::where('school_id', $school->id)->get();
         $news = News::where('user_id', $school->user_id)->latest()->get();
         $transportations = Transportation::where('school_id', $school->id)->get();
