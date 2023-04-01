73c235dd156e3d0d77e3214b4c1d60b86504529e
OmarMarie <36051823+OmarMarie@users.noreply.github.com>
2023-04-01 23:25:38 +0300

    Update SchoolController.php

--- app/Http/Controllers/SchoolController.php
+++ app/Http/Controllers/SchoolController.php
@@ -135,7 +135,8 @@ class SchoolController extends Controller
         $cleanSlug = str_replace('-', ' ', $slug);
         $cleanSlug = rtrim($cleanSlug);
         $school = School::where('name_ar', 'like', '%'.$cleanSlug.'%')->orWhere('name_en', 'like', '%'.$cleanSlug.'%')->first();
-        $genderSchool = explode(',', $school->gender);
+        if($school->gender)
+            $genderSchool = explode(',', $school->gender);
         $gallery = GallarySchool::where('school_id', $school->id)->get();
         $news = News::where('user_id', $school->user_id)->latest()->get();
         $transportations = Transportation::where('school_id', $school->id)->get();
