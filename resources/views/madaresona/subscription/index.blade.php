12ba2f77a16c00a88f2af8f857dd3966fbb11a04
OmarMarie <36051823+OmarMarie@users.noreply.github.com>
2023-05-21 22:11:31 +0300

    Update index.blade.php

--- resources/views/madaresona/subscription/index.blade.php
+++ resources/views/madaresona/subscription/index.blade.php
@@ -1,17 +1,4 @@
 @extends('layouts.main')
-<head>
-    <title>{{(app()->getLocale() == 'ar') ? 'دليل مدارسنا التعليمي | اشترك الان' : 'Our Schools Educational Guide | subscribe now'}}</title>
-     <meta name="description" itemprop="description" content="دليل مدارس الاردن , دليل المراكز التعليمية الاردن, دليل حضانات الاردن , دليل روضات الاردن , دليل المدارس الخاصة عمان">
-     
-    
-    <meta property="og:title" content="{{(app()->getLocale() == 'ar') ? 'دليل مدارسنا التعليمي | اشترك الان' : 'Our Schools Educational Guide | subscribe now'}}">
-    <meta property="og:description" content="دليل مدارس الاردن , دليل المراكز التعليمية الاردن, دليل حضانات الاردن , دليل روضات الاردن , دليل المدارس الخاصة عمان">
-    <meta property="og::url" content="https://madaresonajo.com/{{app()->getLocale()}}/newSubscription">
-    <meta property="og:image" content="https://madaresonajo.com/assets/images/favicon.png">
-    <meta property="og:image:width" content="300">
-    <meta property="og:image:height" content="200">
-</head>
-
 @section('content')
 
 
