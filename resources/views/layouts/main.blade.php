8f7f51183d041570babe5bd1fe0569b53c5deaf1
OmarMarie <36051823+OmarMarie@users.noreply.github.com>
2023-05-21 22:11:22 +0300

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -27,6 +27,17 @@
         <meta property="og:image" content="{{ asset('assets/images/favicon.png') }}"/>
         <meta property="og:image:width" content="300">
         <meta property="og:image:height" content="200">
+        
+    @elseif((Request::segment(2)) != "newSubscription")
+            <title>{{(app()->getLocale() == 'ar') ? 'دليل مدارسنا التعليمي | اشترك الان' : 'Our Schools Educational Guide | subscribe now'}}</title>
+            <meta name="description" itemprop="description" content="دليل مدارس الاردن , دليل المراكز التعليمية الاردن, دليل حضانات الاردن , دليل روضات الاردن , دليل المدارس الخاصة عمان">
+            <meta property="og:title" content="{{(app()->getLocale() == 'ar') ? 'دليل مدارسنا التعليمي | اشترك الان' : 'Our Schools Educational Guide | subscribe now'}}">
+            <meta property="og:description" content="دليل مدارس الاردن , دليل المراكز التعليمية الاردن, دليل حضانات الاردن , دليل روضات الاردن , دليل المدارس الخاصة عمان">
+            <meta property="og::url" content="https://madaresonajo.com/{{app()->getLocale()}}/newSubscription">
+            <meta property="og:image" content="https://madaresonajo.com/assets/images/favicon.png">
+            <meta property="og:image:width" content="300">
+            <meta property="og:image:height" content="200">
+            
     @endif
 
     {{--<link href="cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.min.css" rel="stylesheet" type="text/css" />--}}
