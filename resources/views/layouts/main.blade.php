e9aab463211170b831d78252e7c72e8a52d14156
OmarMarie96 <76706087+OmarMarie96@users.noreply.github.com>
2023-10-12 12:47:11 +0300

    Meta Pixel Code

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -92,6 +92,24 @@
    {{--  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>--}}
     <script src="{{asset('respond.min.js')}}"></script>
     <![endif]-->
+    
+     <!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1572109733617631');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1572109733617631&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->
 
 </head>
 
@@ -158,6 +176,7 @@
 
     gtag('config', 'UA-63566876-1');
 </script>
+
 
 </body>
 
